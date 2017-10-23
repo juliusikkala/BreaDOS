f08d8db8dd66fa3366db62244d77836c9a616d05
Julius Ikkala <julius.ikkala@gmail.com>
2017-10-23 14:52:47 +0300

    Added GRUB to the setup script

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,11 +4,11 @@ cd "${0%/*}"
 
 GCC="gcc-7.2.0"
 BINUTILS="binutils-2.29"
+GRUB="grub-2.02~rc2"
 
 PREFIX=$PWD
 TARGET=i686-elf
 PATH="$PREFIX/bin:$PATH"
-
 export PATH
 
 if [ ! -e ${BINUTILS} ]; then
@@ -22,6 +22,7 @@ if [ ! -e ${BINUTILS} ]; then
     make -j32
     make install
     popd
+    rm -rf build-binutils
 fi
 
 if [ ! -e ${GCC} ]; then
@@ -41,6 +42,31 @@ if [ ! -e ${GCC} ]; then
     make install-gcc
     make install-target-libgcc
     popd
+    rm -rf build-gcc
+fi
+
+if [ ! -e ${GRUB} ]; then
+    wget -nH --cut-dirs=2 -m ftp://alpha.gnu.org/gnu/grub/${GRUB}.tar.xz -O grub
+    tar -xJf grub
+    rm grub
+
+    pushd ${GRUB}
+    ./autogen.sh
+    popd
+
+    mkdir build-grub
+    pushd build-grub
+    ../${GRUB}/configure
+    make dist -j32
+    cp ${GRUB}.tar.gz ..
+    tar -zxvf ${GRUB}.tar.gz
+    pushd ${GRUB}
+    ./configure --prefix="$PREFIX"
+    make -j32
+    make install
+    popd
+    popd
+    rm -rf build-grub
 fi
 
 PREFIX_ESC=$(echo $PWD | sed 's_/_\\/_g')
